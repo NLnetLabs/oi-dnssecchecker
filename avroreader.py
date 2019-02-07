f0161e896a719522f564dd28119ade5cf66f4757
Roland van Rijswijk-Deij <roland@nlnetlabs.nl>
2019-02-07 14:11:39 +0000

    Added TLSA check

--- avroreader.py
+++ avroreader.py
@@ -18,11 +18,11 @@ import dnssecchecks
 ##
 
 def logstats(logger, stats_dict):
-	keys = list(stats_dict.keys())
-	keys.sort()
+    keys = list(stats_dict.keys())
+    keys.sort()
 
-	for key in keys:
-		logger.log_info('{}: {}'.format(key, stats_dict[key]))
+    for key in keys:
+        logger.log_info('{}: {}'.format(key, stats_dict[key]))
 
 ##
 # Function to read record from the specified Avro file;
@@ -30,62 +30,64 @@ def logstats(logger, stats_dict):
 # contains all the records for a single name
 ##
 
-def read_avro(filename, domrecs_callback, result_dict, stats_dict, skip = 0):
-	logger = oilog.OILog()
-	logger.open_for_avro(filename)
-	recs = 0
-	avro_fd = open(filename, 'rb')
+def read_avro(filename, tld, domrecs_callback, result_dict, stats_dict, skip = 0):
+    tld = '{}.'.format(tld)
+    logger = oilog.OILog()
+    logger.open_for_avro(filename)
+    recs = 0
+    avro_fd = open(filename, 'rb')
 
-	avro_reader = fastavro.reader(avro_fd)
+    avro_reader = fastavro.reader(avro_fd)
 
-	previous_fqdn = None
-	rec_dict = dict()
-	qname = None
+    previous_fqdn = None
+    rec_dict = dict()
+    qname = None
 
-	if skip > 0:
-		logger.log_info("Skipping {} records".format(skip))
+    if skip > 0:
+        logger.log_info("Skipping {} records".format(skip))
 
-	for record in avro_reader:
-		recs += 1
+    for record in avro_reader:
+        recs += 1
 
-		if recs < skip:
-			continue
+        if recs < skip:
+            continue
 
-		if recs % 100000 == 0:
-			logger.log_info('Read {} records from {}'.format(recs, filename))
-			logstats(logger, stats_dict)
+        if recs % 100000 == 0:
+            logger.log_info('Read {} records from {}'.format(recs, filename))
+            logstats(logger, stats_dict)
 
-		qname = record['query_name']
+        qname = record['query_name']
 
-		if qname.startswith('www.'):
-			qname = qname[4:]
+        if qname.startswith('www.'):
+            qname = qname[4:]
 
-		if qname.startswith('123-nonexistant-dnsjedi-456.'):
-			qname = qname[28:]
+        if qname.startswith('123-nonexistant-dnsjedi-456.'):
+            qname = qname[28:]
 
-		if previous_fqdn is not None and qname != previous_fqdn:
-			domrecs_callback(logger, previous_fqdn, rec_dict, result_dict, stats_dict)
-			rec_dict = dict()
-			previous_fqdn = qname
-		elif previous_fqdn == None:
-			previous_fqdn = qname
+        if previous_fqdn is not None and qname != previous_fqdn:
+            if previous_fqdn.endswith(tld):
+                domrecs_callback(logger, previous_fqdn, rec_dict, result_dict, stats_dict)
+            rec_dict = dict()
+            previous_fqdn = qname
+        elif previous_fqdn == None:
+            previous_fqdn = qname
 
-		dnsrec = oidnstypes.avro_rec_to_dnstype(logger, record)
+        dnsrec = oidnstypes.avro_rec_to_dnstype(logger, record)
 
-		if dnsrec is None:
-			continue
+        if dnsrec is None:
+            continue
 
-		cur_recs = rec_dict.get((record['query_name'], record['query_type']), [])
-		cur_recs.append(dnsrec)
+        cur_recs = rec_dict.get((record['query_name'], record['query_type']), [])
+        cur_recs.append(dnsrec)
 
-		rec_dict[(record['query_name'], record['query_type'])] = cur_recs
+        rec_dict[(record['query_name'], record['query_type'])] = cur_recs
 
-	if qname is not None:
-		domrecs_callback(logger, qname, rec_dict, result_dict, stats_dict)
+    if qname is not None and qname.endswith(tld):
+        domrecs_callback(logger, qname, rec_dict, result_dict, stats_dict)
 
-	logger.log_info('Read {} records from {}'.format(recs, filename))
+    logger.log_info('Read {} records from {}'.format(recs, filename))
 
-	logstats(logger, stats_dict)
+    logstats(logger, stats_dict)
 
-	avro_fd.close()
-	logger.close()
+    avro_fd.close()
+    logger.close()
--- execute_checks.py
+++ execute_checks.py
@@ -20,104 +20,133 @@ import threading
 import multiprocessing as mp
 import json
 
-def avro_check_proc(logger, avro_queue, avro_dir, out_dir):
-	while not avro_queue.empty():
-		try:
-			avro_name = avro_queue.get()
-		except queue.Empty as e:
-			break
+def avro_check_proc(logger, avro_queue, avro_dir, out_dir, tld, tlsa_one_set, tlsa_all_set):
+    while not avro_queue.empty():
+        try:
+            avro_name = avro_queue.get()
+        except queue.Empty as e:
+            break
 
-		logger.log_info('Starting on {}'.format(avro_name))
+        logger.log_info('Starting on {}'.format(avro_name))
 
-		result_dict = dict()
-		stats_dict = dict()
+        result_dict = dict()
+        stats_dict = dict()
 
-		#try:
-		avroreader.read_avro('{}/{}'.format(avro_dir, avro_name), dnssecchecks.domain_data_callback, result_dict, stats_dict)
-		#except Exception as e:
-		#	logger.log_err('An exception occurred while processing {}'.format(avro_name))
-		#	logger.log_err(e)
-		#	logger.log_err('Will not write results for {}'.format(avro_name))
-		#	raise e
-			#continue
+        try:
+            avroreader.read_avro('{}/{}'.format(avro_dir, avro_name), tld, dnssecchecks.domain_data_callback, result_dict, stats_dict)
+        except Exception as e:
+            logger.log_err('An exception occurred while processing {}'.format(avro_name))
+            logger.log_err(e)
+            logger.log_err('Will not write results for {}'.format(avro_name))
+            raise e
 
-		out_name = avro_name.replace('.avro','.json')
-		stats_name = avro_name.replace('.avro', '-stats.json')
+        out_name = avro_name.replace('.avro','.json')
+        stats_name = avro_name.replace('.avro', '-stats.json')
 
-		out_fd = open('{}/{}'.format(out_dir, out_name), 'w')
+        out_fd = open('{}/{}'.format(out_dir, out_name), 'w')
 
-		for k in result_dict.keys():
-			result = dict()
-			result[k] = result_dict[k]
-			out_fd.write('{}\n'.format(json.dumps(result)))
+        for k in result_dict.keys():
+            dom_result = result_dict[k]
 
-		out_fd.close()
+            if k in tlsa_one_set:
+                dom_result['tlsa_one_mx'] = True
 
-		stats_fd = open('{}/{}'.format(out_dir, stats_name), 'w')
+            if k in tlsa_all_set:
+                dom_result['tlsa_all_mx'] = True
 
-		stats_fd.write('{}\n'.format(json.dumps(stats_dict)))
+            result = dict()
+            result[k] = dom_result
+            out_fd.write('{}\n'.format(json.dumps(result)))
 
-		stats_fd.close()
+        out_fd.close()
 
-		logger.log_info('Done processing {}'.format(avro_name))
-		logger.log_info('Wrote results for {} to {}/{}'.format(avro_name, out_dir, out_name))
-		logger.log_info('Wrote statistics for {} to {}/{}'.format(avro_name, out_dir, stats_name))
+        stats_fd = open('{}/{}'.format(out_dir, stats_name), 'w')
 
-def process_avro_files(logger, avro_dir, proc_count, out_dir):
-	avro_list = []
+        stats_fd.write('{}\n'.format(json.dumps(stats_dict)))
 
-	for f in os.listdir(avro_dir):
-		if f.lower().endswith('.avro'):
-			avro_list.append(f)
+        stats_fd.close()
 
-	logger.log_info('Found {} Avro files in {}'.format(len(avro_list), avro_dir))
-	logger.log_info('Writing results to {}'.format(out_dir))
-	oilog.mkdir_p(out_dir)
+        logger.log_info('Done processing {}'.format(avro_name))
+        logger.log_info('Wrote results for {} to {}/{}'.format(avro_name, out_dir, out_name))
+        logger.log_info('Wrote statistics for {} to {}/{}'.format(avro_name, out_dir, stats_name))
 
-	analysis_queue = mp.Queue()
+def process_avro_files(logger, avro_dir, proc_count, out_dir, tld, tlsa_one_set, tlsa_all_set):
+    avro_list = []
 
-	for a in avro_list:
-		analysis_queue.put(a)
+    for f in os.listdir(avro_dir):
+        if f.lower().endswith('.avro'):
+            avro_list.append(f)
 
-	analysis_procs = set()
+    logger.log_info('Found {} Avro files in {}'.format(len(avro_list), avro_dir))
+    logger.log_info('Writing results to {}'.format(out_dir))
+    oilog.mkdir_p(out_dir)
 
-	for t in range(0, proc_count):
-		analysis_procs.add(mp.Process(target=avro_check_proc, args=(logger, analysis_queue, avro_dir, out_dir)))
+    analysis_queue = mp.Queue()
 
-	logger.log_info('Starting analysis processes')
+    for a in avro_list:
+        analysis_queue.put(a)
 
-	for t in analysis_procs:
-		t.start()
+    analysis_procs = set()
 
-	while len(analysis_procs) > 0:
-		for t in analysis_procs:
-			t.join(0.25)
-			if not t.is_alive():
-				analysis_procs.remove(t)
-				break
+    for t in range(0, proc_count):
+        analysis_procs.add(mp.Process(target=avro_check_proc, args=(logger, analysis_queue, avro_dir, out_dir, tld, tlsa_one_set, tlsa_all_set)))
+
+    logger.log_info('Starting analysis processes')
+
+    for t in analysis_procs:
+        t.start()
+
+    while len(analysis_procs) > 0:
+        for t in analysis_procs:
+            t.join(0.25)
+            if not t.is_alive():
+                analysis_procs.remove(t)
+                break
+
+def load_tlsa_list(list_file):
+    tlsa_set = set()
+
+    try:
+        tlsa_fd = open(list_file, 'r')
+
+        for line in tlsa_fd:
+            tlsa_set += line.strip('\r').strip('\n')
+
+        tlsa_fd.close()
+    except Exception as e:
+        logger.log_err('Failed to load domains with TLSA records from {} ({})'.format(list_file, e))
+
+    return tlsa_set
 
 def main():
-	argparser = argparse.ArgumentParser(description='Perform DNSSEC checks against Avro files in a directory')
+    argparser = argparse.ArgumentParser(description='Perform DNSSEC checks against Avro files in a directory')
 
-	argparser.add_argument('-d,--directory', nargs=1, help='the directory containing Avro files', type=str, metavar='avro_dir', dest='avro_dir', required=True)
-	argparser.add_argument('-p,--processes', nargs=1, help='number of processes to start', type=int, default=1, metavar='proc_count', dest='proc_count', required=False)
-	argparser.add_argument('-l,--logdir', nargs=1, help='output directory for logs', type=str, default='.', metavar='log_dir', dest='log_dir', required=False)
-	argparser.add_argument('-o,--output-dir', nargs=1, help='output directory for analysis results', type=str, default='.', metavar='out_dir', dest='out_dir', required=False)
+    argparser.add_argument('-d, --directory', nargs=1, help='the directory containing Avro files', type=str, metavar='avro_dir', dest='avro_dir', required=True)
+    argparser.add_argument('-p, --processes', nargs=1, help='number of processes to start', type=int, default=1, metavar='proc_count', dest='proc_count', required=False)
+    argparser.add_argument('-l, --logdir', nargs=1, help='output directory for logs', type=str, default='.', metavar='log_dir', dest='log_dir', required=False)
+    argparser.add_argument('-o, --output-dir', nargs=1, help='output directory for analysis results', type=str, default='.', metavar='out_dir', dest='out_dir', required=False)
+    argparser.add_argument('-t, --tlsa-one', nargs=1, help='file containing domains with at least one MX record with a corresponding TLSA record', type=str, metavar='tlsa_one', dest='tlsa_one', required=True)
+    argparser.add_argument('-T, --tlsa-all', nargs=1, help='file containing domains where all MX records have a corresponding TLSA record', type=str, metavar='tlsa_all', dest='tlsa_all', required=True)
+    argparser.add_argument('-D, --tld', nargs=1, help='TLD to process', type=str, metavar='tld', dest='tld', required=True)
 
-	args = argparser.parse_args()
+    args = argparser.parse_args()
 
-	oilog.set_log_dir(args.log_dir[0])
+    oilog.set_log_dir(args.log_dir[0])
 
-	logger = oilog.OILog()
-	logger.open('oi-dnssecchecks-{}.log'.format(datetime.date.today()))
+    logger = oilog.OILog()
+    logger.open('oi-dnssecchecks-{}.log'.format(datetime.date.today()))
 
-	try:
-		process_avro_files(logger, args.avro_dir[0], args.proc_count[0], args.out_dir[0])
-	except Exception as e:
-		logger.log_err('Process terminated with an exception')
-		logger.log_err(e)
+        # Load TLSA sets
+        tlsa_one_set = load_tlsa_list(args.tlsa_one[0])
+        tlsa_all_set = load_tlsa_list(args.tlsa_all[0])
 
-	logger.close()
+    try:
+        process_avro_files(logger, args.avro_dir[0], args.proc_count[0], args.out_dir[0], args.tld[0], tlsa_one_set, tlsa_all_set)
+    except Exception as e:
+        logger.log_err('Process terminated with an exception')
+        logger.log_err(e)
+
+    logger.close()
 
 if __name__ == "__main__":
-	main()
+    main()
