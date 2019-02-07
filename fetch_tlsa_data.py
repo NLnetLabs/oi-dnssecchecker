c1bbf46caab01850410db7333d07a42d0679cd3b
Roland van Rijswijk-Deij <roland@nlnetlabs.nl>
2019-02-07 13:27:59 +0000

    Added script to fetch TLSA data from OpenINTEL

---
+++ fetch_tlsa_data.py
@@ -0,0 +1,216 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2019 NLnet Labs
+# Licensed under a 3-clause BSD license, see LICENSE in the
+# distribution
+#
+# Main module that will execute the checks for all Avro files
+# it can find in the directory specified on the command line.
+# Will spawn the specified number of threads, which defaults
+# to a single thread if no thread count is specified.
+
+import os
+import sys
+import datetime
+import argparse
+import json
+from impala.dbapi import connect
+import shutil
+
+config = None
+
+def fail(msg, exit_code = 1):
+    sys.stderr.write('{}\n'.format(msg))
+    sys.exit(exit_code)
+
+def load_config(cfgfile):
+    global config
+
+    try:
+        config_fd = open(cfgfile, 'r')
+        config = json.load(config_fd)
+        config_fd.close()
+    except Exception as e:
+        fail('Failed to load configuration from {} ({})'.format(cfgfile, e))
+
+def get_config_item(item, default_value = None):
+    global config
+
+    if item not in config:
+        if default_value is None:
+            fail("Could not find mandatory item '{}' in the configuration".format(item))
+
+        return default_value
+
+    return config[item]
+
+def conn_impala():
+    try:
+        krb_principal   = get_config_item("kerberos.principal")
+        krb_keytab      = get_config_item("kerberos.keytab")
+
+        res = os.system('kinit -k -t {} {}'.format(krb_keytab, krb_principal))
+
+        if res != 0:
+            raise('Failed to obtain a Kerberos ticket')
+
+        # OpenINTEL main node, connect over TLS with Kerberos authentication
+        oi_host = get_config_item("openintel.access_host")
+        oi_port = int(get_config_item("openintel.access_port"))
+
+        conn = connect(host=oi_host, port=oi_port, auth_mechanism='GSSAPI', use_ssl=True)
+
+        return conn
+    except Exception as e:
+        fail("Failed to connect to the OpenINTEL cluster, giving up. ({})".format(e))
+
+def set_requestpool(cur):
+    pool = get_config_item("openintel.request_pool", "none")
+
+    if pool == "none":
+        return
+
+    try:
+        cur.execute('SET REQUEST_POOL="{}";'.format(pool))
+    except Exception as e:
+        fail('Failed to select Impala request pool ({})'.format(e))
+
+def fetch_tlsa_data():
+    conn = conn_impala()
+
+    # We always process data for the day before
+    day = datetime.date.today() - datetime.timedelta(days = 1)
+
+    dataset = get_config_item("partner.dataset")
+    tld = get_config_item("partner.tld")
+
+    output_dir = get_config_item("output_dir")
+
+    try:
+        os.makedirs(output_dir, exist_ok=True)
+        print('Created {}'.format(output_dir))
+    except Exception as e:
+        fail('Failed to create {} ({})'.format(output_dir, e))
+
+    cur = conn.cursor()
+
+    set_requestpool(cur)
+
+    # Fetch domains with at least one TLSA record and an RRSIG over the TLSA record
+    query = ''
+    query += 'SELECT DISTINCT query_name\n'
+    query += 'FROM openintel.{}_warehouse_parquet\n'.format(dataset)
+    query += 'WHERE year="{:04d}" AND month="{:02d}" AND day="{:02d}"\n'.format(day.year, day.month, day.day)
+    query += 'AND mx_address IS NOT NULL\n'
+    query += 'AND query_name LIKE "%.{}."\n'.format(tld)
+    query += 'AND mx_address IN (SELECT DISTINCT regexp_extract(query_name, "(_[^\.]+\._[^\.]+\.)(.*$)", 2) AS mx_host_name\n'
+    query += '                   FROM openintel.tlsa_warehouse_parquet\n'
+    query += '                   WHERE year="{:04d}" AND month="{:02d}" AND day="{:02d}"\n'.format(day.year, day.month, day.day)
+    query += '                   AND query_type="TLSA"\n'
+    query += '                   AND response_type="TLSA")\n'
+    query += 'AND mx_address IN (SELECT DISTINCT regexp_extract(query_name, "(_[^\.]+\._[^\.]+\.)(.*$)", 2) AS mx_host_name\n'
+    query += '                   FROM openintel.tlsa_warehouse_parquet\n'
+    query += '                   WHERE year="{:04d}" AND month="{:02d}" AND day="{:02d}"\n'.format(day.year, day.month, day.day)
+    query += '                   AND query_type="TLSA"\n'
+    query += '                   AND response_type="RRSIG");\n'
+
+    mark = datetime.datetime.now()
+
+    sys.stdout.write('Fetching domains with at least one MX record with a TLSA record for {} from {} ... '.format(tld, dataset))
+    sys.stdout.flush()
+
+    try:
+        cur.execute(query)
+    except Exception as e:
+        print("FAILED")
+        fail('Failed to execute Impala query {} ({})'.format(query, e))
+
+    now = datetime.datetime.now()
+
+    print('OK ({})'.format(now - mark))
+
+    output_name = '{}/{}-{}.txt'.format(output_dir, get_config_item('tlsa_one_mx_prefix'), day)
+
+    try:
+        output_fd = open(output_name, 'w')
+
+        for row in cur:
+            output_fd.write('{}\n'.format(row[0]))
+
+        output_fd.close()
+    except Exception as e:
+        fail('Failed to fetch query results and write them to {} ({})'.format(output_name, e))
+
+    # Now find domains where all MX records have a corresponding TLSA record
+    query = ''
+    query += 'SELECT DISTINCT t1.query_name\n'
+    query += 'FROM (SELECT DISTINCT query_name\n'
+    query += '      FROM openintel.{}_warehouse_parquet\n'.format(dataset)
+    query += '      WHERE year="{:04d}" AND month="{:02d}" AND day="{:02d}"\n'.format(day.year, day.month, day.day)
+    query += '      AND mx_address IS NOT NULL\n'
+    query += '      AND query_name LIKE "%.{}."\n'.format(tld)
+    query += '      AND mx_address IN (SELECT DISTINCT regexp_extract(query_name, "(_[^\.]+\._[^\.]+\.)(.*$)", 2) AS mx_host_name\n'
+    query += '                         FROM openintel.tlsa_warehouse_parquet\n'
+    query += '                         WHERE year="{:04d}" AND month="{:02d}" AND day="{:02d}"\n'.format(day.year, day.month, day.day)
+    query += '                         AND query_type="TLSA"\n'
+    query += '                         AND response_type="TLSA")\n'
+    query += '      AND mx_address IN (SELECT DISTINCT regexp_extract(query_name, "(_[^\.]+\._[^\.]+\.)(.*$)", 2) AS mx_host_name\n'
+    query += '                         FROM openintel.tlsa_warehouse_parquet\n'
+    query += '                         WHERE year="{:04d}" AND month="{:02d}" AND day="{:02d}"\n'.format(day.year, day.month, day.day)
+    query += '                         AND query_type="TLSA"\n'
+    query += '                         AND response_type="RRSIG")) AS t1\n'
+    query += 'LEFT ANTI JOIN\n'
+    query += '(SELECT DISTINCT query_name\n'
+    query += ' FROM openintel.{}_warehouse_parquet\n'.format(dataset)
+    query += ' WHERE year="{:04d}" AND month="{:02d}" AND day="{:02d}"\n'.format(day.year, day.month, day.day)
+    query += ' AND mx_address IS NOT NULL\n'
+    query += ' AND query_name LIKE "%.{}."\n'.format(tld)
+    query += ' AND mx_address NOT IN (SELECT DISTINCT regexp_extract(query_name, "(_[^\.]+\._[^\.]+\.)(.*$)", 2) AS mx_host_name\n'
+    query += '                        FROM openintel.tlsa_warehouse_parquet\n'
+    query += '                        WHERE year="{:04d}" AND month="{:02d}" AND day="{:02d}"\n'.format(day.year, day.month, day.day)
+    query += '                        AND query_type="TLSA"\n'
+    query += '                        AND response_type="TLSA")) AS t2\n'
+    query += 'ON t1.query_name = t2.query_name;\n'
+
+    mark = datetime.datetime.now()
+
+    sys.stdout.write('Fetching domains where all MX records have a corresponding TLSA record for {} from {} ... '.format(tld, dataset))
+    sys.stdout.flush()
+
+    try:
+        cur.execute(query)
+    except Exception as e:
+        print("FAILED")
+        fail('Failed to execute Impala query {} ({})'.format(query, e))
+
+    now = datetime.datetime.now()
+
+    print('OK ({})'.format(now - mark))
+
+    output_name = '{}/{}-{}.txt'.format(output_dir, get_config_item('tlsa_all_mx_prefix'), day)
+
+    try:
+        output_fd = open(output_name, 'w')
+
+        for row in cur:
+            output_fd.write('{}\n'.format(row[0]))
+
+        output_fd.close()
+    except Exception as e:
+        fail('Failed to fetch query results and write them to {} ({})'.format(output_name, e))
+
+def main():
+    argparser = argparse.ArgumentParser(description='Extract list of domains with TLSA ')
+
+    argparser.add_argument('-c, --config', nargs=1, help='configuration file to use', type=str, metavar='config_file', dest='config_file', required=True)
+
+    args = argparser.parse_args()
+
+    # Load configuration
+    load_config(args.config_file[0])
+
+    # Fetch TLSA data
+    fetch_tlsa_data()
+
+if __name__ == "__main__":
+    main()
